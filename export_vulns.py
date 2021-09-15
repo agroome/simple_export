cbb1841bc70e35d1d45017f6623ce207f1d24035
Andy Groome <agroome@users.noreply.github.com>
2021-09-15 13:36:09 -0400

    bug fix to regex

--- export_vulns.py
+++ export_vulns.py
@@ -9,15 +9,10 @@ import re
 from pathlib import Path
 from typing import List, Iterator
 
+
 DEFAULT_CHUNK_SIZE = 500
 DEFAULT_POLL_INTERVAL = 3
 BASE_URL = 'https://cloud.tenable.com'
-
-logging.basicConfig(
-    filename='vuln_exporter.log',
-    format='%(asctime)s[%(levelname)s]:%(message)s',
-    level=logging.INFO
-)
 
 default_config_path = Path(f'{__file__}').parent
 default_config_file = '.env'
@@ -30,6 +25,24 @@ get_headers = {'Accept': 'application/json'}
 post_headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
 download_headers = {'Accept': 'application/octet-stream'}
 
+env_template = '''
+ACCESS_KEY=9a09bb6a8f...
+SECRET_KEY=0f262ed976...
+
+rename_columns:
+    asset.ipv4                   : IP
+    asset.hostname               : Hostname
+    asset.operating_system       : OS
+    plugin.name                  : Vulnerability Title
+    severity                     : Severity
+    plugin.cvss_base_score       : CVSS Score
+    plugin.exploit_available     : Exploit Exists? (YES/NO)
+    state                        : Vulnerability status (Fixed/Active)
+    first_found                  : Date Vuln was detected
+    last_fixed                   : Vuln Remediation date
+
+'''
+
 
 def rename_columns(records: Iterator[dict], name_map: dict) -> List[dict]:
     """Using the name_map dictionary, map the column name when an entry matches."""
@@ -40,31 +53,34 @@ def rename_columns(records: Iterator[dict], name_map: dict) -> List[dict]:
 def read_env_file(filename: str) -> dict:
     """Read a simple environment file that contains api_keys and new column header names.
     :param filename
-    :returns: dictionary with keys: 'api_keys' and 'rename_fields'
+    :returns: dictionary with keys: 'api_keys' and 'column_names'
     """
     keys_dict = dict()
     field_dict = dict()
     keys_regex = '^\\s*(?P<token>access_key|secret_key)\\s*=\\s*(?P<value>[0-9a-z]+)\\s*$'
-    fields_regex = '^\\s*(?P<original_field>[a-z_.]+)\\s+:\\s+(?P<new_field>[A-Za-z].*)\\s*$'
+    fields_regex = '^\\s*(?P<original_field>[0-9A-Za-z_.]+)\\s+:\\s+(?P<new_field>.*)\\s*$'
     try:
         with open(filename) as fobj:
             for line in fobj:
+                logging.debug(f'config line: {line}')
 
                 m = re.match(keys_regex, line, re.IGNORECASE)
                 match = m and m.groupdict()
                 if match:
                     keys_dict.update({match['token'].lower(): match['value']})
+                    logging.debug(f'matched keys')
 
                 m = re.match(fields_regex, line, re.IGNORECASE)
                 match = m and m.groupdict()
                 if match:
                     field_dict.update({match['original_field'].strip(): match['new_field'].strip()})
+                    logging.debug(f'matched fields {match["original_field"]} -> {match["new_field"]}')
 
     except Exception as e:
         SystemExit(repr(e))
 
-    logging.debug(f'rename fields: {field_dict}')
-    return {'api_keys': keys_dict, 'rename_columns': field_dict}
+    logging.debug(f'column names: {field_dict}')
+    return {'api_keys': keys_dict, 'column_names': field_dict}
 
 
 class VulnerabilityExportRequest:
@@ -126,29 +142,44 @@ class VulnerabilityExporter:
                 yield chunk
 
 
-def export_to_csv(config_filepath, output_filepath):
+def read_config(config_filepath: str) -> dict:
     """Read from the export iterator, write csv lines to output_filepath"""
     logging.debug(f'reading config from {config_filepath}')
 
     config = read_env_file(config_filepath)
-    api_keys = config.get('api_keys')
-    column_names = config.get('rename_columns')
+
+    try:
+        api_keys = config['api_keys']
+    except KeyError:
+        logging.error('API keys not found. Please create a .env file.')
+        raise SystemExit('API keys not found.')
+
+    column_names = config.get('column_names')
     logging.debug(f'map column names using {column_names}')
 
     if not api_keys:
-        logging.error('API keys not found.')
+        logging.error('API keys not found. Please create a .env file.')
         raise SystemExit('API keys not found.')
 
-    exporter = VulnerabilityExporter(**api_keys)
+    return config
 
+
+def export_vulns(api_keys: dict, column_names: dict) -> Iterator:
+    exporter = VulnerabilityExporter(**api_keys)
+    logging.debug(f'mapping column_names: {column_names}')
     vuln_records = rename_columns(flatten_vuln_export(exporter), name_map=column_names)
-    logging.info(f'Exporting records to {output_filepath}.')
+    return vuln_records
+
+
+def write_to_csv(records: Iterator[dict], fieldnames: List[str], filepath: str):
+    logging.info(f'Exporting records to {filepath}.')
+    logging.debug(f'fieldnames: {fieldnames}')
 
     try:
-        with open(output_filepath, 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=list(column_names.values()))
+        with open(filepath, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            for count, record in enumerate(vuln_records):
+            for count, record in enumerate(records):
                 writer.writerow(record)
             logging.info(f'Exported {count+1} records')
     except Exception as e:
@@ -159,18 +190,24 @@ def export_to_csv(config_filepath, output_filepath):
 def parse_args():
     """Parse command line arguments."""
     parser = ArgumentParser()
-    parser.add_argument('--config-path',
+    parser.add_argument('--in-path',
                         help='Location of the config file.',
                         default=default_config_path)
-    parser.add_argument('--config-file',
+    parser.add_argument('--in-file',
                         help='Name of config file (defaults to .env).',
                         default=default_config_file)
-    parser.add_argument('--output-path',
+    parser.add_argument('--out-path',
                         help='Path to write output file.',
                         default=default_output_path)
-    parser.add_argument('--output-file',
+    parser.add_argument('--out-file',
                         help='Output filename (defaults to "vuln_export_{date_string}.csv").',
                         default=default_output_filename)
+    parser.add_argument('--log-level',
+                        help='Defaults to INFO',
+                        default="INFO")
+    parser.add_argument('--generate-env',
+                        help='Generate an .env file. Edit this file then save to .env',
+                        action='store_true')
     return parser.parse_args()
 
 
@@ -206,9 +243,29 @@ def flatten_vuln_export(records):
 
 def main():
     args = parse_args()
-    config_filepath = Path(args.config_path) / args.config_file
-    output_filepath = Path(args.output_path) / args.output_file
-    return export_to_csv(config_filepath, output_filepath)
+
+    logging.basicConfig(
+        filename='vuln_exporter.log',
+        format='%(asctime)s[%(levelname)s]:%(message)s',
+        level=getattr(logging, args.log_level.upper())
+    )
+    logging.debug(f'loglevel: {args.log_level}')
+
+    if args.generate_env:
+        try:
+            with open('.env', 'w') as _env_template:
+                _env_template.write(env_template)
+        except Exception as e:
+            logging.error(repr(e))
+    else:
+        config_filepath = Path(args.in_path) / args.in_file
+        output_filepath = Path(args.out_path) / args.out_file
+
+        config = read_config(config_filepath)
+        logging.debug(f'config: {config}')
+        vuln_iterator = export_vulns(config['api_keys'], config['column_names'])
+        fieldnames = list(config['column_names'].values())
+        write_to_csv(vuln_iterator, fieldnames, output_filepath)
 
 
 if __name__ == '__main__':
